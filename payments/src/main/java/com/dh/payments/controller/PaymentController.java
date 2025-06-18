2e87ef97517c789e056008a67b5364744d51481a
veronicajujuy <veronica.jujuy@gmail.com>
2025-06-18 18:19:38 -0300

    agregando cambios para poder usar logstash con kibana y elasticsearch

--- payments/src/main/java/com/dh/payments/controller/PaymentController.java
+++ payments/src/main/java/com/dh/payments/controller/PaymentController.java
@@ -2,19 +2,25 @@ package com.dh.payments.controller;
 
 import com.dh.payments.model.Payment;
 import com.dh.payments.service.PaymentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentController {
 
     private PaymentService paymentService;
+    Logger log = LoggerFactory.getLogger(PaymentController.class);
 
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
+    }
+
+    @GetMapping()
+    public Payment getPayment(String id) {
+        log.info("Intentando obtener el pago con id: " + id);
+        return new Payment(id, 400.0f, "id 1", "id 2");
     }
 
     @PostMapping()
